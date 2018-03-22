a313977059083b927406b4b9c84ce2fa86f925d3
YOUR NAME <hardikg23@gmail.com>
2018-03-22 14:37:30 +0530

    changing custom error for oauth/token

--- lib/grape_oauth2/strategies/social_login.rb
+++ lib/grape_oauth2/strategies/social_login.rb
@@ -8,11 +8,18 @@ module Grape
           # Processes Password request.
           def process(request)
             client = authenticate_client(request) || request.invalid_client!
-            resource_owner = authenticate_resource_owner_social(client, request)
+            # resource_owner = authenticate_resource_owner_social(client, request)
+            response = authenticate_resource_owner(client, request)
+            request.unauthorized!('Username or Password is invalid.') if response.blank? 
 
             # request.invalid_grant! if resource_owner.nil?
-            request.unauthorized!('invalid id or password') if resource_owner.nil?
+            # request.unauthorized!('invalid id or password') if resource_owner.nil?
+            if response[:error] || response[:resource_owner].blank?
+              error = response[:error_message] || 'Username or Password is invalid.'
+              request.unauthorized!(error)
+            end
 
+            resource_owner = response[:resource_owner]
             token = config.access_token_class.create_for(client, resource_owner, scopes_from(request))
             expose_to_bearer_token(token)
           end
